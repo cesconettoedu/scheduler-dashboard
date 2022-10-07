d9e1454fcca19eb001b5c66cd58f285472970a56
Eduardo <cesconettoedu@gmail.com>
2022-10-06 23:23:45 -0400

    before add function Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,35 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+
+
+
+
 
 class Dashboard extends Component {
 
@@ -9,16 +38,41 @@ class Dashboard extends Component {
     loading: false
   };
 
+  state = {
+    focused: null
+  };
 
+
+  
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+   }
+
+   
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
 
-    return <main className={dashboardClasses} />;
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+   .map(panel => (
+      <Panel  
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
