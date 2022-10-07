326d0f7c9e6e44d6991c979060804bb6a9fc8500
Eduardo <cesconettoedu@gmail.com>
2022-10-07 09:46:28 -0400

    binding this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -42,9 +42,9 @@ class Dashboard extends Component {
  
  
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
+    this.setState(previousState => ({
+     focused: previousState.focused !== null ? null : id
+    }));
    }
 
  
